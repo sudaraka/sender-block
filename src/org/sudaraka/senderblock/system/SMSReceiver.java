f858e65a9d5d3753be850ac7ce6003cfe6c344f0
Sudaraka Wijesinghe <sudaraka.wijesinghe@gmail.com>
2013-06-05 19:42:11 +0530

    Adding the actual "block" function
    SMS broadcast receiver will lookup the blocked senders list and if the send is
    in there, it will stop the broadcast from propagating.
    
    Trick here is that the priority of this broadcast receiver's intent filter is
    to the maximum (2^31 - 1). It is not determined how this will behave when a
    application with same priority listens to the same intent.

--- src/org/sudaraka/senderblock/system/SMSReceiver.java
+++ src/org/sudaraka/senderblock/system/SMSReceiver.java
@@ -1,6 +1,7 @@
 package org.sudaraka.senderblock.system;
 
 import org.sudaraka.senderblock.R;
+import org.sudaraka.senderblock.data.BlockedSendersSMS;
 import org.sudaraka.senderblock.data.MessagesSMS;
 import org.sudaraka.senderblock.ui.MessageFilterStatActivity;
 
@@ -13,6 +14,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsMessage;
+import android.util.Log;
 
 
 public class SMSReceiver extends BroadcastReceiver {
@@ -22,28 +24,36 @@ public class SMSReceiver extends BroadcastReceiver {
 		Intent i = new Intent(context, MessageFilterStatActivity.class);
 		PendingIntent pi = PendingIntent.getActivity(context, 0, i, 0);
 
-		Notification notification = new NotificationCompat.Builder(context)
-			.setContentTitle("Got SMS")
-			.setContentText("Details not available")
-			.setSmallIcon(R.drawable.ic_launcher)
-			.setContentIntent(pi)
-			.build()
-			;
-		notification.flags |= Notification.FLAG_AUTO_CANCEL;
-
 		NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-		nm.notify(0, notification);
 
 		Bundle b = intent.getExtras();
 		if(null != b) {
 			Object[] pdus = (Object[]) b.get("pdus");
 			SmsMessage[] msg_list = new SmsMessage[pdus.length];
 			MessagesSMS stat = new MessagesSMS(context);
+			BlockedSendersSMS blocked = new BlockedSendersSMS(context);
 
 			for(int idx = 0; idx < msg_list.length; idx++) {
 				msg_list[idx] = SmsMessage.createFromPdu((byte[]) pdus[idx]);
+				
+				String sender = msg_list[idx].getOriginatingAddress();
 
-				stat.add(msg_list[idx].getOriginatingAddress(), (int) msg_list[idx].getTimestampMillis() / 1000);
+				stat.add(sender, (int) msg_list[idx].getTimestampMillis() / 1000);
+				
+				if(blocked.exists(sender)) {
+					abortBroadcast();
+					
+					Notification notification = new NotificationCompat.Builder(context)
+						.setContentTitle("SMS blocked from " + sender)
+						.setContentText("Touch to see the blocked list.")
+						.setSmallIcon(R.drawable.blocked)
+						.setContentIntent(pi)
+						.build()
+						;
+					notification.flags |= Notification.FLAG_AUTO_CANCEL;
+					
+					nm.notify(0, notification);
+				}
 			}
 		}
 	}
