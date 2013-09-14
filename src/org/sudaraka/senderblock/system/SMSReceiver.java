3f505ff5af064bfe84c4603e6bf7172772752253
Sudaraka Wijesinghe <sudaraka.wijesinghe@gmail.com>
2013-09-14 14:55:05 +0530

    [fix] Maniftest incompatibility with Android 4.1.2
    *Issue*
    
    Permission being in the application/receiver/intent-filter caused the intent to
    be ignored in Android 4.1.2 based CM for Xperia E (dual).
    
    *Solution*
    
    Remove the permission attribute from intent-filter.

--- src/org/sudaraka/senderblock/system/SMSReceiver.java
+++ src/org/sudaraka/senderblock/system/SMSReceiver.java
@@ -14,7 +14,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsMessage;
-import android.util.Log;
 
 
 public class SMSReceiver extends BroadcastReceiver {
